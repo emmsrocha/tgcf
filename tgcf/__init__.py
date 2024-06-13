1b5bee53f96a0fa0f34c81280de923d5b2ae6c8a
emmsrocha <emmsrocha@gmail.com>
2024-06-13 08:04:23 -0300

    Update __init__.py

--- tgcf/__init__.py
+++ tgcf/__init__.py
@@ -1,9 +1,8 @@
 """Package tgcf.
-
 The ultimate tool to automate custom telegram message forwarding.
 https://github.com/aahnik/tgcf
 """
 
 from importlib.metadata import version
 
-__version__ = version(__version__)
+__version__ = version(__package__)
